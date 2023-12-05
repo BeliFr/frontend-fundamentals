f288c52fda5f26f5cd2c3fa0bbd8efa7987dc69e
Bere <bere.frias@imagendigital.com>
2023-12-04 19:19:48 -0600

    add logo

--- proyecto_front_gastos/src/components/Header.jsx
+++ proyecto_front_gastos/src/components/Header.jsx
@@ -3,6 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import logo from "../images/logowealth.png"
 
 
 
@@ -11,11 +12,14 @@ const Header = () => {
     return(
       <AppBar
       position="fixed"
+      
       sx={{
         background: 'linear-gradient(to right, #a6a6ff, #5E62D1)', 
         borderRadius: '0 0 50% 0',
         height: '10rem',
       }}
+      
+    
     >
       <Toolbar
         sx={{
@@ -26,6 +30,17 @@ const Header = () => {
           height: '100%', 
         }}
       >
+        <img
+        src={logo}
+        alt=""
+        style={{
+          width: '100px', 
+          height: '100px', 
+          marginTop:'40px',
+          marginBottom: '10px',
+          borderRadius: '50%', 
+        }}
+        />
         <Typography
           color="white"
           component="div"
@@ -35,7 +50,7 @@ const Header = () => {
             fontSize: "44px",
           }}
         >
-          Aplicación de gastos
+          WealthWave
         </Typography>
       </Toolbar>
     </AppBar>
